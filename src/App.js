7fa774e4af8650e1db837e95e6c854d0249da5e7
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2022-12-27 13:26:40 +0200

    design the menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import 'normalize.css'
+import './border-box.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <aside className = "sidemenu">
+        <div className = "sidemenu-button">
+          <span className = "sidemenu-button-span">
+            +
+          </span>
+          New Chat
+
+        </div>
+      </aside>
+      <section className = "chatbox">
+        <div className = "chat-input-holder">
+          <textarea 
+            rows = "1"
+            className = "chat-input-textarea" 
+            placeholder = "type your messsage here">
+          </textarea>
+        </div>
+      </section>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +38,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
