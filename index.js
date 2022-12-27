951a017aa9741ee91e4f86c5207b933841cb0ded
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2022-12-27 20:39:24 +0200

    start on backend

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Configuration, OpenAIApi } =  require ("openai");
+
+const express = require('express');
+const Joi = require('joi');
+const multer = require('multer');
+const cors = require('cors')
+const mysql = require('mysql')
+const app = express();
+app.use(express.json());
+
+
+const fetch = import("node-fetch");
+
+const configuration = new Configuration({
+    organization: "org-zU0hhpIWZhem6uNpsh11cuOk",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai =new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+// callApi()
+async function callApi () {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "hello world",
+        temperature: 0.6,
+    });
+    console.log(completion.data.choices);
+    return completion;
+}
+
+app.get("/", async (req, res) => {
+    const  completion = await callApi ();
+    console.log(completion.data.choices[0].text);
+    return res.json({
+        data: completion.data
+    })
+})
+
+app.listen(4000,() =>{
+    console.log('product server listening on port 4000')
+});
